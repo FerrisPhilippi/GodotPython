2b2f1cf320c180f8511dac2b0546593e34ccd40d
FerrisPhilippi <PhilippiFerris@gmail.com>
2024-06-29 12:03:45 +0200

    parte string to dictionary in python

--- addons/python_connector/ws.py
+++ addons/python_connector/ws.py
@@ -1,11 +1,14 @@
 import asyncio
 import websockets
+import ast
 
 
 async def server(ws, path):
     async for msg in ws:
         msg = msg.decode('utf-8')
-        print(f"Msg from client: {msg}")
+        # print(f"Msg from client: {msg}")
+        dictionary = ast.literal_eval(msg)
+        print(dictionary)
         await ws.send(f"Got your message: {msg}")
 
 start_server = websockets.serve(server, "localhost", 6000)
